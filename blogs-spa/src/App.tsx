90a131b8e42119a3cc64bbe669f0923175eb5c1a
fahad-md-kamal <faahad.hossain@gmail.com>
2023-05-05 23:43:19 +0600

    Frontend and backend Project Architecture

---
+++ blogs-spa/src/App.tsx
@@ -0,0 +1,11 @@
+// import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold text-red-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga repellendus dolorem doloremque eos aut eius ea, sint perspiciatis eligendi adipisci consequatur explicabo, ipsa inventore sequi cupiditate aspernatur labore, at reiciendis?</h1>
+  )
+}
+
+export default App
